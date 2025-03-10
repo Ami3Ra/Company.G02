238ce434da68ad555c893e60fdf026cb6cdb3774
Amira-Amr-25 <amiraamr445@gmail.com>
2025-03-10 17:32:50 +0200

    Department Module BLL

---
+++ Company.G02.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.G02.BLL.Interfaces;
+using Company.G02.DAL.Data.Contexts;
+using Company.G02.DAL.Models;
+
+namespace Company.G02.BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context;  // NULL
+
+        // ASK CLR CREAT Object From CompanyDbContext
+        public DepartmentRepository(CompanyDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+
+
+        public Department? Get(int id)
+        {
+
+            return _context.Departments.Find(id);
+        }
+
+
+        public int Add(Department model)
+        {
+            _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+
+
+        public int Delete(Department model)
+        {
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+
+    }
+}
